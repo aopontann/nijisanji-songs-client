9fc86906c1acbb98c60afe6c633e9df9340cc378
aopontann <aopontan0416@gmail.com>
2021-06-03 23:11:20 +0900

    update vtuber一覧

--- pages/vtuber/index.js
+++ pages/vtuber/index.js
@@ -1,15 +1,29 @@
 import Link from "next/link";
-import styled from "styled-components"
+import styled from "styled-components";
+import Header from "../../components/header";
 
-export default function FirstPost({ data }) {
+export default function VtuberList({ data }) {
+  const Div = styled.div`
+    margin: 0em;
+    padding: 0rem;
+  `;
+  const Main = styled.main`
+    background-color: #eaf0f6;
+  `;
+  const H2 = styled.h2`
+    text-align: center;
+    padding-top: 1.8rem;
+  `;
   const Ul = styled.ul`
     display: flex;
     flex-wrap: wrap;
     list-style: none;
+    justify-content: center;
   `;
   const Li = styled.li`
     width: 8rem;
-    background-color: olive;
+    height: 12rem;
+    background-color: white;
     margin: 0.5rem;
     text-align: center;
     border-radius: 20px;
@@ -21,40 +35,74 @@ export default function FirstPost({ data }) {
     margin-top: 1rem;
   `;
   const Name = styled.p`
-    background-color: wheat;
+    margin: 1rem;
   `;
-  console.log("data", data);
   return (
-    <>
-      <h1>First Post</h1>
-      <Ul>
-      {data.map(dt => {
-        return (
-          <Li>
-            <Image src={dt.vtuberImage.length ? dt.vtuberImage[0] : "/images/profile.jpg"} />
-            <Name>{dt.name}</Name>
-          </Li>
-        )
-      })}
-      </Ul>
-    </>
+    <Div>
+      <Header />
+      <Main>
+        <H2>ライバー 一覧</H2>
+        <Ul>
+          {data.map((dt) => {
+            if (dt.affiliation == "にじさんじ") {
+              return (
+                <Link href={`/vtuber/${dt.name}`}>
+                  <a>
+                    <Li>
+                      <Image
+                        src={
+                          dt.vtuberImage.length
+                            ? dt.vtuberImage[0]
+                            : "/images/profile.jpg"
+                        }
+                      />
+                      <Name>{dt.name}</Name>
+                    </Li>
+                  </a>
+                </Link>
+              );
+            }
+          })}
+        </Ul>
+        <H2>卒業したライバー</H2>
+        <Ul>
+          {data.map((dt) => {
+            if (dt.affiliation == "にじさんじ卒業") {
+              return (
+                <Link href={`/vtuber/${dt.name}`}>
+                  <a>
+                    <Li>
+                      <Image
+                        src={
+                          dt.vtuberImage.length
+                            ? dt.vtuberImage[0]
+                            : "/images/profile.jpg"
+                        }
+                      />
+                      <Name>{dt.name}</Name>
+                    </Li>
+                  </a>
+                </Link>
+              );
+            }
+          })}
+        </Ul>
+      </Main>
+    </Div>
   );
 }
 
 export async function getStaticProps() {
-  const params = {affiliation: "にじさんじ卒業"};
+  const params = { affiliation: "にじさんじ,にじさんじ卒業" };
   const query = new URLSearchParams(params);
-  const res = await fetch(
-    `http://localhost:8081/vtuber?${query}`,
-    {
-      method: "GET",
-    }
-  );
+  const res = await fetch(`http://localhost:8081/vtuber?${query}`, {
+    method: "GET",
+  });
   const data = await res.json();
 
   return {
     props: {
-      data
-    }
-  }
+      data,
+    },
+  };
 }
