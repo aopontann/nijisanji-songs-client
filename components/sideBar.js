2a93c73e6a6e005e740bb88095e02686852cc935
aopontann <aopontan0416@gmail.com>
2021-06-10 22:02:37 +0900

    add: today_songs

--- components/sideBar.js
+++ components/sideBar.js
@@ -32,6 +32,11 @@ export default function SideBar() {
           <P>ランキング</P>
         </a>
       </Link>
+      <Link href="/today_songs">
+        <a>
+          <P>今日公開動画</P>
+        </a>
+      </Link>
     </Side>
   );
 }
