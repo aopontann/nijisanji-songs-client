4dd45df23e4b7b233287b1eecaa3f9cda81ec484
aopontann <aopontan0416@gmail.com>
2021-06-17 16:53:20 +0900

    update: startTime 取得する条件に公開日を指定できる機能を追加

--- pages/editor/index.js
+++ pages/editor/index.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Layout from "../../components/Layout";
 import { useState } from "react";
 import VideoInfo from "./videoInfo";
+import { get_time } from "../../lib/get_times";
 
 const H1 = styled.h1`
   text-align: center;
@@ -13,20 +14,6 @@ const Ul = styled.div`
   list-style: none;
   align-items: center;
 `;
-const Video = styled.li`
-  display: flex;
-  background-color: white;
-  border-radius: 20px;
-  margin-top: 0.5rem;
-  width: 40rem;
-`;
-const Img = styled.img`
-  margin: 1rem;
-`;
-const VideoControl = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
 let search_songConfirm = true;
 let search_checkSongVtuber = true;
@@ -34,11 +21,19 @@ let search_checkSongVtuber = true;
 export default function Edit() {
   const Address = "http://localhost:8081";
   const [videos, setVideo] = useState([]);
+  const [startAtAfter, setStartAtAfter] = useState(
+    get_time({ format: "YYYY-MM-DDT00:00:00" }) + "Z"
+  );
+  const [startAtBefore, setStartAtBefore] = useState(
+    get_time({ format: "YYYY-MM-DDT23:59:59" }) + "Z"
+  );
 
-  const get_videos = async() => {
+  const get_videos = async () => {
     const params = {
       songConfirm: search_songConfirm,
       checkSongVtuber: search_checkSongVtuber,
+      startAtAfter,
+      startAtBefore,
       maxResults: 10,
       page: 1,
     };
@@ -50,31 +45,45 @@ export default function Edit() {
     console.log("data", data);
     setVideo([...data]);
   };
-  const delete_videos = async() => {
-
-  }
 
   return (
     <Layout>
       <H1>編集ページ</H1>
       <p>このページで動画データの編集ができるよ</p>
       <br />
-      songConfirm:
-      <select onChange={(e) => { search_songConfirm = e.target.value == "true" ? true : false }}>
-        <option value={search_songConfirm ? "true" : "false"}>{search_songConfirm ? "true" : "false"}</option>
-        <option value={search_songConfirm ? "false" : "true"}>{search_songConfirm ? "false" : "true"}</option>
+      歌ってみた動画:
+      <select
+        onChange={(e) => {
+          search_songConfirm = e.target.value == "true" ? true : false;
+        }}
+      >
+        <option value={search_songConfirm ? "true" : "false"}>
+          {search_songConfirm ? "true" : "false"}
+        </option>
+        <option value={search_songConfirm ? "false" : "true"}>
+          {search_songConfirm ? "false" : "true"}
+        </option>
       </select>
-      checkSongVtuber:
-      <select onChange={(e) => { search_checkSongVtuber = e.target.value == "true" ? true : false}}>
-        <option value={search_checkSongVtuber ? "true" : "false"}>{search_checkSongVtuber ? "true" : "false"}</option>
-        <option value={search_checkSongVtuber ? "false" : "true"}>{search_checkSongVtuber ? "false" : "true"}</option>
+      参加ライバーチェック:
+      <select
+        onChange={(e) => {
+          search_checkSongVtuber = e.target.value == "true" ? true : false;
+        }}
+      >
+        <option value={search_checkSongVtuber ? "true" : "false"}>
+          {search_checkSongVtuber ? "true" : "false"}
+        </option>
+        <option value={search_checkSongVtuber ? "false" : "true"}>
+          {search_checkSongVtuber ? "false" : "true"}
+        </option>
       </select>
-      <button onClick={get_videos}>読み込み</button>
+      <br />
+      <input type="text" defaultValue={startAtAfter} onBlur={ (e) => { setStartAtAfter(e.target.value) }}/>
+      <input type="text" defaultValue={startAtBefore} onBlur={ (e) => { setStartAtBefore(e.target.value) }}/>
+      <button onClick={get_videos}>取得</button>
       <Ul>
         {videos.map((dt) => {
-          return (
-            <VideoInfo data={dt}/>
-          )
+          return <VideoInfo data={dt} />;
         })}
       </Ul>
     </Layout>
