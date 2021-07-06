a130c3a8d12ffb676adcec61a0508289ce60f3d1
aopontann <aopontan0416@gmail.com>
2021-07-06 23:02:13 +0900

    update: ranking material-uiを使用

--- pages/ranking.js
+++ pages/ranking.js
@@ -1,82 +1,52 @@
 import Layout from "../components/Layout";
-import styled from "styled-components";
 import { useState } from "react";
-
-const H1 = styled.h1``;
-const Ul = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  list-style: none;
-  align-items: center;
-`;
-const Video = styled.li`
-  background-color: white;
-  border-radius: 20px;
-  margin-top: 0.5rem;
-  width: 30rem;
-`;
-const Img = styled.img``;
-const P = styled.p`
-  margin: 0.5rem;
-`;
-const P_page = styled.p`
-  text-decoration:underline;
-`;
+import { Box } from "@material-ui/core";
+import ImgMediaCard from "../components/card";
+import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
 
 export default function Home({ data }) {
   const [videos, setVideo] = useState(data.slice(0, 50));
-  const [page, setPage] = useState(0);
-  //const [videos, setVideo] = useState(data);
+  const [page, setPage] = useState(1);
 
   const pageUp = () => {
-    console.log("up-rank", rank); //初回51
-    setVideo(data.slice(rank, rank + 50));
+    console.log("up", page);
+    setVideo(data.slice(page*50, (page+1)*50));
     setPage(page + 1);
   };
   const pageDown = () => {
-    console.log("down-rank", rank);
-    if (page > 0) {
-      setVideo(data.slice(rank - 100, rank - 50));
+    console.log("down", page);
+    if (page > 1) {
+      setVideo(data.slice((page-2)*50, (page-1)*50));
       setPage(page - 1);
     } else {
       setVideo(data.slice(0, 50));
-      setPage(0);
+      setPage(1);
     }
   };
 
-  let rank = 0 + page * 50;
-
   return (
     <Layout>
-      <H1>累計視聴回数ランキング</H1>
-      <P_page onClick={pageUp}>
-        {rank + 51 < data.length
-          ? `next(${rank + 51}位 ~ ${rank + 100}位)`
-          : ""}
-      </P_page>
-      <P_page onClick={pageDown}>
-        {rank == 0
-          ? ""
-          : `back(${rank - 50 + 1}位 ~ ${rank})位`}
-      </P_page>
-      <Ul>
-        {videos.map((dt) => {
+      <Typography variant="h5">累計視聴回数ランキング</Typography>
+      <Button variant="outlined" onClick={pageDown}>Back</Button>
+      <Button variant="outlined" onClick={pageUp}>Next</Button>
+      <Typography variant="h6" align="center">{`${(page-1)*50+1}位 〜 ${(page)*50}位`}</Typography>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        p={1}
+        m={1}
+        bgcolor="background.paper"
+        justifyContent="center"
+      >
+        {videos.map(video => {
           return (
-            <Video>
-              <p>{++rank}位</p>
-              <a
-                target="_blank"
-                href={`https://www.youtube.com/watch?v=${dt.id}`}
-              >
-                <Img src={dt.thumbnail.medium} />
-              </a>
-              <P>{dt.title}</P>
-              <p>視聴回数: {dt.statistic.viewCount} 回</p>
-            </Video>
-          );
+            <Box m={1}>
+              <ImgMediaCard video={video} />
+            </Box>
+          )
         })}
-      </Ul>
+      </Box>
     </Layout>
   );
 }
