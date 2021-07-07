a5b54ac66d8c088eef48f97a2eba2845a849b4fd
aopontann <aopontan0416@gmail.com>
2021-07-07 22:54:39 +0900

    add: material-ui material-uiを採用

--- pages/today_songs.js
+++ pages/today_songs.js
@@ -1,6 +1,12 @@
 import Layout from "../components/Layout";
 import styled from "styled-components";
 import { get_time, toDatetime } from "../lib/get_times";
+import { useState } from "react";
+import { Box } from "@material-ui/core";
+import ImgMediaCard from "../components/card";
+import { makeStyles } from "@material-ui/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
 
 const H1 = styled.h1``;
 const Ul = styled.div`
@@ -26,36 +32,33 @@ const P = styled.p`
 export default function Home({ data }) {
   return (
     <Layout>
-      <H1>今日公開予定歌動画</H1>
-      {data.length > 0 ? (
-        <Ul>
-          {data.map((dt) => {
-            const startTime = toDatetime({
-              time: dt.startTime,
-              format: "公開時間: HH時mm分"
-            })
-            return (
-              <Video>
-                <a
-                  target="_blank"
-                  href={`https://www.youtube.com/watch?v=${dt.id}`}
-                >
-                  <Img src={dt.thumbnail.medium} />
-                </a>
-                <P>{dt.title}</P>
-                <P>{startTime}</P>
-              </Video>
-            );
-          })}
-        </Ul>
-      ) : (
-        <p>{`現在時点(${get_time({format: "HH時mm分"})})では今日公開される歌ってみた動画はないみたいだよ`}</p>
-      )}
-    </Layout>
+      <Typography variant="h5">今日公開予定歌動画</Typography>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        p={1}
+        m={1}
+        bgcolor="background.paper"
+        justifyContent="center"
+      >
+        {data.map(video => {
+          return (
+            <Box m={1}>
+              <ImgMediaCard video={video} type={"startTime"} />
+            </Box>
+          )
+        })}
+      </Box>
+  </Layout>
   );
 }
 
 export async function getStaticProps() {
+  // <p>{`現在時点(${get_time({format: "HH時mm分"})})では今日公開される歌ってみた動画はないみたいだよ`}</p>
+  /* const startTime = toDatetime({
+    time: dt.startTime,
+    format: "公開時間: HH時mm分"
+  })*/
   const Address = process.env.API_ADDRESS;
   const today_first = get_time({
     format: "YYYY-MM-DDT00:00:00",
