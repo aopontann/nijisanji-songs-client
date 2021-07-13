2b1ab91284bd16b0c11468ee5e8c048557741df3
aopontann <aopontan0416@gmail.com>
2021-07-13 21:01:17 +0900

    create: about サイト紹介ページを作成

--- pages/about.js
+++ pages/about.js
@@ -5,12 +5,33 @@ import { Typography } from "@material-ui/core";
 export default function About() {
   return (
     <Layout>
-      <Typography variant="h5">このサイトについて</Typography>
-      <br />
-      <Typography variant="body">
-        このサイトは、にじさんじファンが作成したにじさんじの歌まとめサイトです
+      <Typography variant="h5" paragraph>
+        このサイトについて
       </Typography>
-      <br />
+      <Typography variant="subtitle1">
+        どんなサイト？
+      </Typography>
+      <Typography variant="body" paragraph>
+        にじさんじライバーの歌ってみた動画(オリ曲も)をまとめた非公式サイトです
+      </Typography>
+      <Typography variant="subtitle1">
+        このサイトの目的
+      </Typography>
+      <Typography variant="body" paragraph>
+        にじさんじライバーの歌ってみた動画を探しやすくするために作成しました！<br />
+        また、このサイトを使ってくれた人が「このライバーさん、この曲歌っているんだ。知らなかった！」
+        と思ってもらって色んなにじさんじライバーを知るきっかけになったらいいなと思っています。
+      </Typography>
+      <Typography variant="subtitle1">
+        ユーザへのお願い
+      </Typography>
+      <Typography variant="body" paragraph>
+        このサイトで紹介している動画にはタグがついています。そのタグは誰でも編集できるようになっています。<br />
+        タグをつけることで動画を探しやすくなるため、動画の出演ライバーやイラスト提供者などのタグを登録してくれるとありがたいです。
+      </Typography>
+      <Typography variant="body">
+        このサイトは、自分の趣味で個人で運営しています。色々気付かないことが沢山あると思いますので、「こういった機能が欲しい！」、「ここなんかバグってるんだけど...」、「この曲載ってないんだけど(怒)」などありましたら、問い合わせから連絡ください！
+      </Typography>
       
     </Layout>
   );
