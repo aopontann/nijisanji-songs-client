899ddc018bcc389146c05e5b13bb054f3dcd3adb
aopontann <aopontan0416@gmail.com>
2021-08-11 18:10:34 +0900

    update: videoコンポーネント化

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,11 +1,9 @@
 import { get_time, toDatetime } from "../lib/get_times";
-import { useState } from "react";
+import { VideoCard } from "../components/video";
+import EditTagDialog from "../components/editTagDialog";
+import { RecoilRoot } from "recoil";
 import { makeStyles } from "@material-ui/styles";
 import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import Card from "@material-ui/core/Card"
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
@@ -15,23 +13,12 @@ const useStyles = makeStyles((theme) => ({
     },
     textAlign: "center",
   },
-  videos: {
-    width: 345 * 0.8,
-    height: 310 * 0.75,
-    margin: theme.spacing(0.5),
-  },
-  title: {
-    display: "-webkit-box",
-    overflow: "hidden",
-    "-webkit-line-clamp": 2,
-    "-webkit-box-orient": "vertical",
-  },
 }));
 
-export default function Home({ data, update_time }) {
+export default function Home({ data, update_time, address }) {
   const classes = useStyles();
   return (
-    <div>
+    <RecoilRoot>
       <Typography variant="h5">今日公開予定歌動画</Typography>
       <Typography variant="body2" color="textSecondary" component="p">
         {`更新時間:${update_time}`}
@@ -44,45 +31,17 @@ export default function Home({ data, update_time }) {
         bgcolor="background.paper"
         justifyContent="center"
       >
-        {data.map((video) => {
-          const startTime = toDatetime({
-            time: video.startTime,
-            format: "公開時間: HH時mm分",
-          });
-          return (
-            <Card className={classes.videos}>
-              <Link
-                href={`https://www.youtube.com/watch?v=${video.id}`}
-                target="_blank"
-                rel="noopener"
-                underline="none"
-              >
-                <CardMedia
-                  component="img"
-                  alt={video.title}
-                  image={video.thumbnail.medium || ""}
-                  title={video.title}
-                />
-              </Link>
-
-              <CardContent>
-                <Typography className={classes.title}>
-                  <Box lineHeight={1.1}>{video.title}</Box>
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {startTime}
-                </Typography>
-              </CardContent>
-            </Card>
-          );
-        })}
+        {data.map((video) => (
+        <VideoCard video={video} type="statistics" />
+      ))}
       </Box>
       {data.length == 0 ? (
         <Typography variant="body" align="center">{`現時点(${update_time})では、今日公開される動画はないみたいだよ`}</Typography>
       ) : (
         ""
       )}
-    </div>
+      <EditTagDialog address={address} />
+    </RecoilRoot>
   );
 }
 
@@ -115,6 +74,7 @@ export async function getStaticProps() {
     props: {
       data,
       update_time,
+      address: Address,
     },
     revalidate: 60,
   };
