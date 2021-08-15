8082b6fc53b4449a78250663806c73b670c032e1
aopontann <aopontan0416@gmail.com>
2021-08-15 19:22:14 +0900

    analyticsテスト2

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { GA_TRACKING_ID } from "../lib/gtag";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import theme from "../src/theme";
@@ -15,26 +14,11 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          {GA_TRACKING_ID && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-        `,
-                }}
-              />
-            </>
-          )}
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": ${NEXT_PUBLIC_ANALYTICS_ID}}'
+          ></script>
         </Head>
         <body>
           <Main />
