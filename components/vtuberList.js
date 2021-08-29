0e8821b48c3d5f10a3f9b0c5bea100b5bddd35b3
aopontann <aopontan0416@gmail.com>
2021-08-29 16:50:58 +0900

    fix: ライバーと関係のない動画が表示されてしまうバグを修正

--- components/vtuberList.js
+++ components/vtuberList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import { all_videoListState, filtered_videoListState } from "./videoList";
+import {
+  all_videoListState,
+  filtered_videoListState,
+  thisPageState,
+} from "./videoList";
 import { searchCheckBoxState, searchValueState } from "./searchVideos";
 import { vtuberListExpandedState } from "./accordion";
 import { makeStyles } from "@material-ui/styles";
@@ -21,76 +23,80 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function VtuberList({ videos, vtuberList }) {
+export default function VtuberList({ vtuberList }) {
   const all_videoList = useRecoilValue(all_videoListState);
   const searchCheckBox = useRecoilValue(searchCheckBoxState);
   const set_filtered_videoListState = useSetRecoilState(filtered_videoListState);
+  const setThisPage = useSetRecoilState(thisPageState);
   const set_vtuberListExpandedState = useSetRecoilState(vtuberListExpandedState);
-  const [searchValue, setSearchValue] = useRecoilState(searchValueState);
+  const setSearchValue = useSetRecoilState(searchValueState);
   const classes = useStyles();
 
   const tagClick = (event) => {
-    setSearchValue(event.target.textContent);
     const reg = new RegExp(event.target.textContent);
     const result = all_videoList.filter(
       (video) =>
         video.title.match(reg) ||
         (searchCheckBox ? video.description.match(reg) : false) ||
-        video.tags.map((tagData) => tagData.name).includes(searchValue)
+        video.tags
+          .map((tagData) => tagData.name)
+          .includes(event.target.textContent)
     );
     set_filtered_videoListState([...result]);
+    setSearchValue(event.target.textContent);
+    setThisPage(1);
     set_vtuberListExpandedState(false);
   };
 
   return (
     <div>
       {["にじさんじ", "NIJISANJI KR", "NIJISANJI ID", "NIJISANJI EN"].map(
-          (affi, index) => {
-            const filtered_vtuberList = vtuberList.filter(
-              (vtuber) => vtuber.affiliation === affi && vtuber.type === null
-            );
+        (affi, index) => {
+          const filtered_vtuberList = vtuberList.filter(
+            (vtuber) => vtuber.affiliation === affi && vtuber.type === null
+          );
+          return (
+            <div key={index}>
+              <Typography variant="body2" component="p" gutterBottom>
+                {affi}
+              </Typography>
+              <Typography component="ui" className={classes.chips}>
+                {filtered_vtuberList.map((vtuber) => {
+                  return (
+                    <li key={vtuber.name}>
+                      <Chip
+                        size="small"
+                        className={classes.chip}
+                        label={vtuber.name}
+                        onClick={tagClick}
+                      />
+                    </li>
+                  );
+                })}
+              </Typography>
+            </div>
+          );
+        }
+      )}
+      <Typography variant="body2" component="p" gutterBottom>
+        {"卒業したライバー"}
+      </Typography>
+      <Typography component="ui" className={classes.chips}>
+        {vtuberList
+          .filter((vtuber) => vtuber.type == "卒業")
+          .map((vtuber) => {
             return (
-              <div key={index}>
-                <Typography variant="body2" component="p" gutterBottom>
-                  {affi}
-                </Typography>
-                <Typography component="ui" className={classes.chips}>
-                  {filtered_vtuberList.map((vtuber) => {
-                    return (
-                      <li key={vtuber.name}>
-                        <Chip
-                          size="small"
-                          className={classes.chip}
-                          label={vtuber.name}
-                          onClick={tagClick}
-                        />
-                      </li>
-                    );
-                  })}
-                </Typography>
-              </div>
+              <li key={vtuber.name}>
+                <Chip
+                  size="small"
+                  className={classes.chip}
+                  label={vtuber.name}
+                  onClick={tagClick}
+                />
+              </li>
             );
-          }
-        )}
-        <Typography variant="body2" component="p" gutterBottom>
-          {"卒業したライバー"}
-        </Typography>
-        <Typography component="ui" className={classes.chips}>
-          {vtuberList
-            .filter((vtuber) => vtuber.type == "卒業")
-            .map((vtuber) => {
-              return (
-                <li key={vtuber.name}>
-                  <Chip
-                    size="small"
-                    className={classes.chip}
-                    label={vtuber.name}
-                    onClick={tagClick}
-                  />
-                </li>
-              );
-            })}
-        </Typography>
+          })}
+      </Typography>
     </div>
-  )
+  );
 }
