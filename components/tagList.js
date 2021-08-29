b00b5da750e58eb1af0b570714f4759c49ef5586
aopontann <aopontan0416@gmail.com>
2021-08-29 16:50:20 +0900

    fix: タグと関係ない動画が検索結果に出てきてしまうバグを修正

--- components/tagList.js
+++ components/tagList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { searchCheckBoxState, searchValueState } from "./searchVideos";
-import { all_videoListState, filtered_videoListState } from "./videoList";
+import { all_videoListState, filtered_videoListState, thisPageState } from "./videoList";
 import { tagsListExpandedState } from "./accordion";
 import { makeStyles } from "@material-ui/styles";
 import Typography from "@material-ui/core/Typography";
@@ -23,21 +23,23 @@ export default function TagList({ tags }) {
   const all_videoList = useRecoilValue(all_videoListState);
   const searchCheckBox = useRecoilValue(searchCheckBoxState);
   const set_filtered_videoListState = useSetRecoilState(filtered_videoListState);
+  const setThisPage = useSetRecoilState(thisPageState);
   const set_tagsListExpandedState = useSetRecoilState(tagsListExpandedState);
-  const [searchValue, setSearchValue] = useRecoilState(searchValueState);
+  const setSearchValue = useSetRecoilState(searchValueState);
   const classes = useStyles();
 
   const tagClick = (event) => {
-    setSearchValue(event.target.textContent);
     const reg = new RegExp(event.target.textContent);
     const result = all_videoList.filter(
       (video) =>
         video.title.match(reg) ||
         (searchCheckBox ? video.description.match(reg) : false) ||
-        video.tags.map((tagData) => tagData.name).includes(searchValue)
+        video.tags.map((tagData) => tagData.name).includes(event.target.textContent)
     );
     set_filtered_videoListState([...result]);
+    setSearchValue(event.target.textContent);
     set_tagsListExpandedState(false);
+    setThisPage(1);
   };
 
   return (
